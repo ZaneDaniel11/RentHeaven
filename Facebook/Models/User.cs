0faa419fb7e25990cf7c9c9b0cd01bf34aa6a738
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-01-05 01:50:34 +0800

    Add Model for users.tb

---
+++ Facebook/Models/User.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facebook.Models
+{
+
+ [Table("users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(30)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(30)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
+
+        public string BirthDate { get; set; }
+
+        public string ProfilePic { get; set; }
+
+        public string CoverPhoto { get; set; }
+
+        [MaxLength(20)]
+        public string Bio { get; set; }
+
+        [MaxLength(20)]
+        public int MobileNumber { get; set; }
+
+        public bool IsVerified { get; set; } = false;
+
+
+        [MaxLength(20)]
+        public string Status { get; set; } = "active";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}

3352e2abaf63ac3f712b8bebf2dd167283896228
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-04-07 15:14:05 +0800

    Replace FileName

--- Facebook/Controllers/AccountController.cs
+++ Facebook/Controllers/AccountController.cs
@@ -1,29 +1,28 @@
 using System;
+using Facebook.Models;
+using Facebook.Models.ViewModels;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Facebook.Models;
-using Facebook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Facebook.Controllers
 {
     [Route("[controller]")]
-    public class RegisterController : Controller
+    public class AccountController : Controller
     {
-        private readonly ILogger<RegisterController> _logger;
+        private readonly ILogger<AccountController> _logger;
 
-        public RegisterController(ILogger<RegisterController> logger)
+        public AccountController(ILogger<AccountController> logger)
         {
             _logger = logger;
         }
 
-        [HttpPost]
         public IActionResult Register(RegisterViewModel model)
         {
-
-        }
+        
+       }
     }
 }
--- Facebook/Models/ViewModels/RegisterViewModel.cs
+++ Facebook/Models/ViewModels/RegisterViewModel.cs
@@ -1,12 +1,34 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Facebook.Models.ViewModels
 {
+   
     public class RegisterViewModel
     {
-        
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        public string BirthDate { get; set; }
     }
 }

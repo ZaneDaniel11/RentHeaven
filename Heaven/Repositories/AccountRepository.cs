e27e004433b3f912cbb83e6888968fae0a08213b
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-04-20 01:51:44 +0800

    Create Account

---
+++ Heaven/Repositories/AccountRepository.cs
@@ -0,0 +1,29 @@
+using Dapper;
+using System.Data.SQLite;
+using Heaven.Models;
+
+namespace Heaven.Repositories
+{
+    public class AccountRepository
+    {
+        private readonly string _connectionString = "Data Source=Rent.db";
+
+        public void Insert(User user)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"
+                    INSERT INTO Users 
+                    (FullName, Email, PasswordHash, PhoneNumber, Bio, CreatedAt, IsHost)
+                    VALUES 
+                    (@FullName, @Email, @PasswordHash, @PhoneNumber, @Bio, @CreatedAt, @IsHost)";
+
+                connection.Execute(query, user);
+
+                connection.Close();
+            }
+        }
+    }
+}

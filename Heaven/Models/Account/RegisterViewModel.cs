be724861c0bc6fb6e457ba5624cae495641c32e3
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-04-19 20:46:15 +0800

    Added Register

---
+++ Heaven/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+
+namespace Heaven.Models.Account
+{
+    public class RegisterViewModel
+        {
+            [Required]
+            public string FullName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [MinLength(6)]
+            public string Password { get; set; }
+
+            [Compare("Password")]
+            public string ConfirmPassword { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public string Bio { get; set; }
+
+            public IFormFile ProfilePicture { get; set; } // for image upload (optional)
+        }
+
+}
---
+++ Heaven/Models/User.cs
@@ -0,0 +1,26 @@
+
+namespace Heaven.Models
+{
+   public class User
+        {
+            public int UserID { get; set; }
+
+            public string FullName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            public string PasswordHash { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public string ProfilePictureUrl { get; set; }
+
+            public string Bio { get; set; }
+
+            public DateTime CreatedAt { get; set; }
+
+            public bool IsHost { get; set; } // Maps to SQLite INTEGER (0 = false, 1 = true)
+        }
+}
